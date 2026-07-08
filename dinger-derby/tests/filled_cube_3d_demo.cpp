18c4540f45e993b9384d08f6f0bfcf0f071fb92f
Namir Chaudry <158531352+Nchaudry123@users.noreply.github.com>
2026-07-08 00:02:18 -0400

    Add filled cube 3D demo

---
+++ dinger-derby/tests/filled_cube_3d_demo.cpp
@@ -0,0 +1,47 @@
+#include <SFML/Graphics.hpp>
+
+#include "../src/math/Matrix4.h"
+#include "../src/math/Vector3.h"
+#include "../src/rendering/Mesh3D.h"
+#include "../src/rendering/SoftwareRenderer3D.h"
+
+int main() {
+    sf::RenderWindow window(
+        sf::VideoMode(sf::Vector2u(1280, 720)),
+        "3D Filled Cube Demo"
+    );
+    window.setFramerateLimit(60);
+
+    Mesh3D cube = Mesh3D::cube();
+    sf::Clock clock;
+
+    while (window.isOpen()) {
+        while (const std::optional event = window.pollEvent()) {
+            if (event->is<sf::Event::Closed>()) {
+                window.close();
+            }
+
+            if (const auto* resized = event->getIf<sf::Event::Resized>()) {
+                window.setView(sf::View(sf::FloatRect(
+                    sf::Vector2f(0.0f, 0.0f),
+                    sf::Vector2f(resized->size.x, resized->size.y)
+                )));
+            }
+        }
+
+        float time = clock.getElapsedTime().asSeconds();
+        Matrix4 transform =
+            Matrix4::rotationY(time * 0.75f) *
+            Matrix4::rotationX(time * 0.45f) *
+            Matrix4::scale(Vector3(1.35f, 1.35f, 1.35f));
+
+        SoftwareRenderer3D renderer(window);
+
+        window.clear(sf::Color(12, 14, 18));
+        renderer.drawMeshTriangles(cube, transform, sf::Color(180, 180, 180));
+        renderer.drawMeshEdges(cube, transform, sf::Color(20, 22, 26));
+        window.display();
+    }
+
+    return 0;
+}
