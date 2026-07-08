9dd9b160c999f200639886676a886d5fd3b6a80a
Namir Chaudry <158531352+Nchaudry123@users.noreply.github.com>
2026-07-08 00:00:00 -0400

    Add triangle faces to Mesh3D

--- dinger-derby/src/rendering/Mesh3D.h
+++ dinger-derby/src/rendering/Mesh3D.h
@@ -9,10 +9,17 @@ struct Edge3D {
     int end = 0;
 };
 
+struct Triangle3D {
+    int a = 0;
+    int b = 0;
+    int c = 0;
+};
+
 class Mesh3D {
 public:
     std::vector<Vector3> vertices;
     std::vector<Edge3D> edges;
+    std::vector<Triangle3D> triangles;
 
     static Mesh3D cube(float size = 2.0f);
     static Mesh3D axes(float length = 1.5f);
--- dinger-derby/tests/mesh3d_test.cpp
+++ dinger-derby/tests/mesh3d_test.cpp
@@ -14,11 +14,15 @@ void testCubeMeshShape() {
 
     assert(cube.vertices.size() == 8);
     assert(cube.edges.size() == 12);
+    assert(cube.triangles.size() == 12);
     assert(nearlyEqual(cube.vertices[0].x, -1.0f));
     assert(nearlyEqual(cube.vertices[0].y, -1.0f));
     assert(nearlyEqual(cube.vertices[0].z, -1.0f));
     assert(cube.edges[0].start == 0);
     assert(cube.edges[0].end == 1);
+    assert(cube.triangles[0].a == 0);
+    assert(cube.triangles[0].b == 2);
+    assert(cube.triangles[0].c == 1);
 }
 
 void testAxesMeshShape() {
@@ -26,6 +30,7 @@ void testAxesMeshShape() {
 
     assert(axes.vertices.size() == 4);
     assert(axes.edges.size() == 3);
+    assert(axes.triangles.empty());
     assert(nearlyEqual(axes.vertices[1].x, 3.0f));
     assert(nearlyEqual(axes.vertices[2].y, 3.0f));
     assert(nearlyEqual(axes.vertices[3].z, 3.0f));
